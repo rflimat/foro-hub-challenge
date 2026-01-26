9b8503be791b17372a70fe698f1b20ecbfae8cc6
rflimat <raulflimat@gmail.com>
2026-01-26 17:44:06 -0500

    Configurando el entorno Java y Spring

---
+++ src/main/java/com/rflimat/foro_hub_challenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rflimat.foro_hub_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rflimat/foro_hub_challenge/ForoHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rflimat.foro_hub_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
