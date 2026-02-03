1d0ab0541b5666120816d73572fe1a321a925bbd
rflimat <raulflimat@gmail.com>
2026-02-02 20:58:32 -0500

    Agregando gestion de excepciones en la aplicación

---
+++ src/main/java/com/rflimat/foro_hub_challenge/common/exception/ValidacionException.java
@@ -0,0 +1,8 @@
+package com.rflimat.foro_hub_challenge.common.exception;
+
+public class ValidacionException extends RuntimeException {
+
+    public ValidacionException(String mensaje) {
+        super(mensaje);
+    }
+}
