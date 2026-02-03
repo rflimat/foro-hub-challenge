09ed10310c670c97b034a459a1b4764bb306da89
rflimat <raulflimat@gmail.com>
2026-02-02 21:08:57 -0500

    Agregando modelo y repositorio de la entidad Curso

---
+++ src/main/java/com/rflimat/foro_hub_challenge/repository/CursoRepository.java
@@ -0,0 +1,7 @@
+package com.rflimat.foro_hub_challenge.repository;
+
+import com.rflimat.foro_hub_challenge.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
