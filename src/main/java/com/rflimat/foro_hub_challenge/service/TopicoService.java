8431a41bd308dbefc54f6407779fcf1edd2667d6
rflimat <raulflimat@gmail.com>
2026-02-02 21:37:21 -0500

    Agregando servicio de Topico

---
+++ src/main/java/com/rflimat/foro_hub_challenge/service/TopicoService.java
@@ -0,0 +1,42 @@
+package com.rflimat.foro_hub_challenge.service;
+
+import com.rflimat.foro_hub_challenge.common.exception.ValidacionException;
+import com.rflimat.foro_hub_challenge.dto.topico.DatosDetalleTopico;
+import com.rflimat.foro_hub_challenge.dto.topico.DatosRegistroTopico;
+import com.rflimat.foro_hub_challenge.model.Topico;
+import com.rflimat.foro_hub_challenge.repository.CursoRepository;
+import com.rflimat.foro_hub_challenge.repository.TopicoRepository;
+import com.rflimat.foro_hub_challenge.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicoService {
+    @Autowired
+    private TopicoRepository topicoRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public DatosDetalleTopico registrar(DatosRegistroTopico datos) {
+        if(datos.idUsuario() != null && !usuarioRepository.existsById(datos.idUsuario())){
+            throw new ValidacionException("No existe un usuario con el id informado");
+        }
+
+        if(datos.idCurso() != null && !cursoRepository.existsById(datos.idCurso())){
+            throw new ValidacionException("No existe un curso con el id informado");
+        }
+
+        if (topicoRepository.existsByTituloOrMensaje(datos.titulo(), datos.mensaje())) {
+            throw new ValidacionException("Ya existe un tópico con el mismo título y/o mensaje");
+        }
+
+        var autor = usuarioRepository.findById(datos.idUsuario()).get();
+        var curso = cursoRepository.findById(datos.idCurso()).get();
+        var topico = new Topico(null, autor, curso, datos);
+        topicoRepository.save(topico);
+
+        return new DatosDetalleTopico(topico);
+    }
+}
